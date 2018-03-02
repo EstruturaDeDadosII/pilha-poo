1804654f44c77b2af9f903a9ec709d88063cb10c
Thiago Cunha <grupocodenome@gmail.com>
2018-03-02 01:56:41 -0300

    adicionando projeto de pilha poo

---
+++ CMakeFiles/pilha_poo.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/thiago/CLionProjects/pilha-poo/main.cpp" "C:/Users/thiago/CLionProjects/pilha-poo/cmake-build-debug/CMakeFiles/pilha_poo.dir/main.cpp.obj"
+  "C:/Users/thiago/CLionProjects/pilha-poo/pilha.cpp" "C:/Users/thiago/CLionProjects/pilha-poo/cmake-build-debug/CMakeFiles/pilha_poo.dir/pilha.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/pilha_poo.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pilha_poo.dir/main.cpp.obj"
+  "CMakeFiles/pilha_poo.dir/pilha.cpp.obj"
+  "pilha_poo.pdb"
+  "pilha_poo.exe"
+  "pilha_poo.exe.manifest"
+  "libpilha_poo.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pilha_poo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
